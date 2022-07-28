6aa9b8bfe66d8efcd088aadc61ea550021eed1b9
Chung Nguyen <zrmedia9@gmail.com>
2022-07-28 21:38:32 +0700

    Add products list in the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import Layout from '../components/Layout';
+import ProductItem from '../components/ProductItem';
 
 export default function Home() {
   return (
     <Layout title={'Home'}>
-      <div>
-        <h1 className="text-3xl font-bold">Next Tailwind Amazona</h1>
-      </div>
+      <ProductItem />
     </Layout>
   );
 }
