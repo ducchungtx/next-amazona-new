fedd52e74c1a2f4d374376822216899911f37ed9
Chung Nguyen <zrmedia9@gmail.com>
2022-07-31 09:27:48 +0700

    Connect to mongodb, seed user list data

---
+++ models/User.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Check existing user model if it exists not create new one else use existing one
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
