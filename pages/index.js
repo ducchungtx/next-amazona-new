4d533e132154b5f5809948aa9437245a79a84945
Chung Nguyen <zrmedia9@gmail.com>
2022-08-01 21:54:43 +0700

    Update the code

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,27 @@
 import Layout from '../components/Layout';
 import ProductItem from '../components/ProductItem';
-import data from '../utils/data';
+import db from '../utils/db';
+import Product from '../models/Product';
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <Layout title={'Home'}>
-      <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
-        {data.products.map((product) => {
-          return <ProductItem key={product.slug} product={product} />;
-        })}
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {products &&
+          products.map((product) => {
+            return <ProductItem key={product.slug} product={product} />;
+          })}
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find().lean();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
 }
